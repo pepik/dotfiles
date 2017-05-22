e80dbcb437d461947b81982e3724266268fddaef
Petr Nyc <jet.jetpac@gmail.com>
2017-05-22 13:10:04 +0200

    Initial vim config.

---
+++ vim/.vim/bundles.sh
@@ -0,0 +1,13 @@
+mkdir bundle
+cd bundle
+
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/altercation/vim-colors-solarized
+git clone https://github.com/tpope/vim-commentary
+git clone https://github.com/tpope/vim-endwise
+git clone https://github.com/tpope/vim-sensible
+git clone https://github.com/jewes/Conque-Shell
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/Logaltog/powerline
+git clone https://github.com/klen/python-mode
+git clone https://github.com/davidhalter/jedi-vim
